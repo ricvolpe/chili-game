a3e351bf57faea64dcfc9e0b709159d892933188
ricvolpe <volpatorv@gmail.com>
2022-11-18 21:25:31 +0000

    Formatted how

--- pages/how.tsx
+++ pages/how.tsx
@@ -7,11 +7,12 @@ export default function Select() {
   return (
     <div className='main'>
       <HomeLink />
-      <p className='title'>
-        How to play
-      </p>
+      <p className='title'>How to play</p>
       <div>
-        <p className='subtitle'>General rules</p>
+        <div className={styles.heading}>
+          <Image src="/imgs/arrowBlack.png" alt="One chili" width={50} height={30} />
+          <p className='subtitle'>General rules</p>
+        </div>
         <ul className={styles.ruleList}>
           <li>Going around, each person selects a spicy level and then, if they feel like it, answers the question that shows up</li>
           <li>Anyone else from the table, if they feel like it, can answer the question</li>
@@ -20,10 +21,9 @@ export default function Select() {
       </div>
       <div>
         <div className={styles.heading}>
-          <p className='subtitle'>Easy mode</p>
-          <div className={styles.imgMode}>
-            <Image src="/imgs/easy.png" alt="One chili" width={50} height={50} />
-          </div>
+          <Image src="/imgs/arrowGreen.png" alt="One chili" width={50} height={30} />
+          <p style={{ color: '#76C42B' }} className='subtitle'>Easy mode</p>
+          <Image src="/imgs/easy.png" alt="One chili" width={50} height={50} />
         </div>
         <ul className={styles.ruleList}>
           <li>Start with Yogurt questions</li>
@@ -32,10 +32,9 @@ export default function Select() {
       </div>
       <div>
         <div className={styles.heading}>
-          <p className='subtitle'>Extra spicy mode</p>
-          <div className={styles.imgMode}>
-            <Image src="/imgs/hard.png" alt="One chili" width={45} height={60} />
-          </div>
+          <Image src="/imgs/arrowRed.png" alt="One chili" width={50} height={30} />
+          <p style={{ color: '#EC174C' }} className='subtitle'>Extra spicy mode</p>
+          <Image src="/imgs/hard.png" alt="One chili" width={45} height={60} />
         </div>
         <ul className={styles.ruleList}>
           <li>A person selects a spicy level and asks the question to someone else, instead of answering it</li>
