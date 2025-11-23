a6bc1fbc0804de5aa50642f35cf315bd875c1aa9
Riccardo Volpato <volpatorv@hotmail.com>
2025-11-23 19:12:20 +0000

    Remove link announcement from index page
    Removed the announcement about the game's new link and custom domains.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,6 @@ export default function Home() {
   return (
     <div className='main'>
       <p className='title'>The Chili Game</p>
-      <p className='quote' style={{ color: '#EC174C' }}>
-        On November 12th 2025, The Chili Game will move to{' '}
-        <a
-          href="https://chili-game.vercel.app"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ textDecoration: 'underline', color: '#EC174C' }}
-        >
-          https://chili-game.vercel.app
-        </a>{' '}
-        (because custom domains cost money and not very many players are financially supporting the game)
-        Bookmark the new link if you want to keep playing :)
-      </p>
       <p className='quote'>Leave behind bland small talk and spicy up getting to know each other</p>
       <Image src="/imgs/main.png" alt={mainImgAltText} width={380} height={380} />
       <div>
